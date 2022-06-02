30ee4b10fb23def8a07ac095e468b01f51325aa8
Duong Vu <duong.t.vu@gmail.com>
2022-06-02 09:26:18 +0200

    Update CBSITS2.sh

--- data/CBSITS2.sh
+++ data/CBSITS2.sh
@@ -21,23 +21,32 @@ cd /data/ProgLang/Python/dnabarcoder/data/CBSITS2
 #If the dataset is very large, then predict the local similarity cut-offs only at the immediate higher taxonomic level:
 ../../dnabarcoder.py predict -i dnabarcoder/CBSITS2.species.diff.fasta -c ITS_20211006.classification -st 0.7 -et 1 -s 0.001 -rank species -prefix CBSITS2 -higherrank genus -minSeqNo 30
 #We can also use the original dataset and remove species complexes during the prediction:
-../../dnabarcoder.py predict -i dnabarcoder/CBSITS2.species.diff.fasta -c ITS_20211006.classification -st 0.7 -et 1 -s 0.001 -rank species -prefix CBSITS2 -higherrank genus -minSeqNo 30
-#Predict a global similarity cut-off at the genus level
+../../dnabarcoder.py predict -i CBSITS2.species.fasta -c ITS_20211006.classification -removecomplexes yes -st 0.7 -et 1 -s 0.001 -rank species -prefix CBSITS2 -higherrank genus -minSeqNo 30
+#Predict a global similarity cutoff for genus identification:
 ../../dnabarcoder.py predict -i CBSITS2.genus.fasta -c ITS_20211006.classification -st 0.7 -et 1 -s 0.001 -rank genus -prefix CBSITS2
+#Predict local cutoffs for genus identification at different taxonomic levels:
 ../../dnabarcoder.py predict -i CBSITS2.genus.fasta -c ITS_20211006.classification -st 0.5 -et 1 -s 0.001 -rank genus -higherrank family,order,class,phylum -prefix CBSITS2 -minseqno 30
+#Predict a global similarity cutoff for family identification:
 ../../dnabarcoder.py predict -i CBSITS2.family.fasta -c ITS_20211006.classification -st 0.5 -et 1 -s 0.001 -rank family -prefix CBSITS2
+#Predict local cutoffs for family identification at different taxonomic levels:
 ../../dnabarcoder.py predict -i CBSITS2.family.fasta -c ITS_20211006.classification -st 0.5 -et 1 -s 0.001 -rank family -higherrank order,class,phylum -prefix CBSITS2 -minseqno 30
+#Predict a global similarity cutoff for order identification:
 ../../dnabarcoder.py predict -i CBSITS2.order.fasta -c ITS_20211006.classification -st 0.5 -et 1 -s 0.001 -rank order -prefix CBSITS2
+#Predict local cutoffs for order identification at different taxonomic levels:
 ../../dnabarcoder.py predict -i CBSITS2.order.fasta -c ITS_20211006.classification -st 0.5 -et 1 -s 0.001 -rank order -higherrank class,phylum -prefix CBSITS2 -minseqno 30
+#Predict local cutoffs for class identification at different taxonomic levels:
 ../../dnabarcoder.py predict -i CBSITS2.class.fasta -c ITS_20211006.classification -st 0.5 -et 1 -s 0.001 -rank class -prefix CBSITS2
+#Predict local cutoffs for class identification at the phylum:
 ../../dnabarcoder.py predict -i CBSITS2.class.fasta -c ITS_20211006.classification -st 0.5 -et 1 -s 0.001 -rank class  -higherrank phylum -prefix CBSITS2 -minseqno 30
-#visualization 
+#Visualization. Here we just simply load the existing prediction for visualization: 
 ../../dnabarcoder.py predict -i CBSITS2.fasta -c ITS_20211006.classification -rank species,genus,family,order,class 
 ../../dnabarcoder.py predict -i CBSITS2.fasta -c ITS_20211006.classification -rank species -higherrank genus -minseqno 30
 ../../dnabarcoder.py predict -i CBSITS2.fasta -c ITS_20211006.classification -rank genus -higherrank family -minseqno 30
 ../../dnabarcoder.py predict -i CBSITS2.fasta -c ITS_20211006.classification -rank family -higherrank order -minseqno 30
 ../../dnabarcoder.py predict -i CBSITS2.fasta -c ITS_20211006.classification -rank order -higherrank class -minseqno 30
 ../../dnabarcoder.py predict -i CBSITS2.fasta -c ITS_20211006.classification -rank class -higherrank phylum  -minseqno 30
+#Visualize the sequence distribution based on DNA comparison (dnabarcoder/CBSITS2.sim). If the similarity matrix is not given, dnabarcoder will compute it.
 ../../dnabarcoder.py visualize -i CBSITS2.fasta  -c ITS_20211006.classification -p 3 -n 10 -size 0.3
+#Compute the best local cutoffs:
 ../../dnabarcoder.py best -i dnabarcoder/CBSITS2.cutoffs.json -c ITS_20211006.classification
 
