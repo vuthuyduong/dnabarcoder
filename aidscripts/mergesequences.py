467f30d89b8b813800d7698ea8850796b01ab17d
Duong Vu <duong.t.vu@gmail.com>
2026-03-13 18:22:46 +0100

    Add files via upload

--- aidscripts/mergesequences.py
+++ aidscripts/mergesequences.py
@@ -3,10 +3,10 @@
 # AUTHOR: Duong Vu
 # CREATE DATE: 07 June 2020
 import sys
-import numpy as np
+
 import os, argparse
 from Bio import SeqIO
-import json
+
 import multiprocessing
 nproc=multiprocessing.cpu_count()
 #from keras.utils import np_utils
@@ -24,18 +24,10 @@ args=parser.parse_args()
 output=args.out
 
 fastafilenames=args.input.split(",")
-seqids=[]
 mergedrecords=[]
-count=0
 for fastafilename in fastafilenames:
 	seqrecords=SeqIO.to_dict(SeqIO.parse(fastafilename, "fasta"))
-	for seqid in seqrecords.keys():
-		if not (seqid in seqids):
-			if count >0 and seqid.startswith("K"):
-				print(seqid)
-			seqids.append(seqid)
-			mergedrecords.append(seqrecords[seqid])
-	count=count+1		
+	mergedrecords=mergedrecords | seqrecords
 #save to file:
 SeqIO.write(mergedrecords,output,"fasta")
 print("All sequences are saved in " + output + ".")
